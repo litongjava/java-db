149361d74129546fd061f257b1ea543090fbe2c9
litongjava <litongjava@qq.com>
2024-11-20 13:02:00 -1000

    update postgresql dialect

--- src/main/java/com/litongjava/db/activerecord/dialect/PostgreSqlDialect.java
+++ src/main/java/com/litongjava/db/activerecord/dialect/PostgreSqlDialect.java
@@ -322,7 +322,34 @@ public class PostgreSqlDialect extends Dialect {
 
   @Override
   public String forDbFindColumnsById(String tableName, String columns, String[] pKeys) {
-    return DialectUtils.forDbFindColumnsById(tableName, columns, pKeys);
+    StringBuilder sql = forDbFindColumnsReturnStrinBuilder(tableName, columns);
+    sql.append(" where ");
+    for (int i = 0; i < pKeys.length; i++) {
+      if (i > 0) {
+        sql.append(" and ");
+      }
+      sql.append('"').append(pKeys[i]).append("\" = ?");
+    }
+    return sql.toString();
+  }
+  
+  public static StringBuilder forDbFindColumnsReturnStrinBuilder(String tableName, String columns) {
+    StringBuilder sql = new StringBuilder("select ");
+    columns = columns.trim();
+    if ("*".equals(columns)) {
+      sql.append('*');
+    } else {
+      String[] arr = columns.split(",");
+      for (int i = 0; i < arr.length; i++) {
+        if (i > 0) {
+          sql.append(',');
+        }
+        sql.append('"').append(arr[i].trim()).append('"');
+      }
+    }
+
+    sql.append(" from \"").append(tableName).append("\"");
+    return sql;
   }
 
   @Override
