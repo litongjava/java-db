3862e5c43a9ff014836762c47b0923860e1d74d5
litongjava <litongjava@qq.com>
2025-03-19 20:54:15 -1000

    add python code engine

---
+++ src/main/java/com/litongjava/template/PythonCodeEngine.java
@@ -0,0 +1,53 @@
+package com.litongjava.template;
+
+import com.jfinal.kit.Kv;
+import com.jfinal.template.Engine;
+import com.jfinal.template.Template;
+import com.litongjava.db.activerecord.Db;
+import com.litongjava.tio.utils.environment.EnvUtils;
+
+public class PythonCodeEngine {
+  public static final String tableName = "llm_python_code";
+  public static final String sql = "select prompt from " + tableName + " where name=? and env=?";
+  public static final String RESOURCE_BASE_PATH = "/python/";
+  public static Engine engine;
+  static {
+    engine = Engine.create("prompt");
+    engine.setBaseTemplatePath(RESOURCE_BASE_PATH);
+    engine.setToClassPathSourceFactory();
+    if (EnvUtils.isDev()) {
+      // 支持模板热加载，绝大多数生产环境下也建议配置成 true，除非是极端高性能的场景
+      engine.setDevMode(true);
+    }
+
+    // 配置极速模式，性能提升 13%
+    Engine.setFastMode(true);
+    // jfinal 4.9.02 新增配置：支持中文表达式、中文变量名、中文方法名、中文模板函数名
+    Engine.setChineseExpression(true);
+
+  }
+
+  public static Template getTemplate(String filename) {
+    return engine.getTemplate(filename);
+  }
+
+  public static String renderToString(String fileName, Kv kv) {
+    return engine.getTemplate(fileName).renderToString(kv);
+  }
+
+  public static String renderToString(String fileName) {
+    return engine.getTemplate(fileName).renderToString();
+  }
+
+  public static String renderToStringFromDb(String fileName) {
+    String queryStr = Db.queryStr(sql, fileName, EnvUtils.env());
+    Template template = engine.getTemplateByString(queryStr);
+    return template.renderToString();
+  }
+
+  public static String renderToStringFromDb(String fileName, Kv kv) {
+    String queryStr = Db.queryStr(sql, fileName, EnvUtils.env());
+    Template template = engine.getTemplateByString(queryStr);
+    return template.renderToString(kv);
+  }
+}
