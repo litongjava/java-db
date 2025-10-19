bf5e092f5872e3bf0d47f127bd5f58b1d7d053c7
Tong Li <litongjava001@gmail.com>
2025-10-18 19:03:26 -1000

    update TemplateEngine

--- src/main/java/com/litongjava/template/TemplateEngine.java
+++ src/main/java/com/litongjava/template/TemplateEngine.java
@@ -14,7 +14,6 @@ public class TemplateEngine {
   static {
     engine = Engine.create("template");
     engine.setBaseTemplatePath(RESOURCE_BASE_PATH);
-    engine.setToClassPathSourceFactory();
 
     if (EnvUtils.isDev()) {
       // 支持模板热加载，绝大多数生产环境下也建议配置成 true，除非是极端高性能的场景
